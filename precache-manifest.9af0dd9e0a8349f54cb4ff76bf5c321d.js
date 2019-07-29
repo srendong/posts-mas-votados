277053553efe257c1dc9b2b4c4ba701528b64754
Samuel Rendon G <s.redon.g@gmail.com>
2019-07-29 17:29:30 -0500

    Updates

---
+++ precache-manifest.9af0dd9e0a8349f54cb4ff76bf5c321d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327380061d5a502ed1dacd0eddcd3d96",
+    "url": "/posts-mas-votados/index.html"
+  },
+  {
+    "revision": "77574ed9dd43dad58b22",
+    "url": "/posts-mas-votados/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8e30cd52cca70ca16500",
+    "url": "/posts-mas-votados/static/css/main.17d26983.chunk.css"
+  },
+  {
+    "revision": "77574ed9dd43dad58b22",
+    "url": "/posts-mas-votados/static/js/2.dc7e8af6.chunk.js"
+  },
+  {
+    "revision": "8e30cd52cca70ca16500",
+    "url": "/posts-mas-votados/static/js/main.63eb749d.chunk.js"
+  },
+  {
+    "revision": "ce6804fa70a3541feaf4",
+    "url": "/posts-mas-votados/static/js/runtime~main.e2023a61.js"
+  }
+]);
